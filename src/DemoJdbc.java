c86282748198db35938283387d5643cf4b41d32e
Mohammed Shafeeq <mohammed.shafeeq@centrico.tech>
2025-08-06 02:32:06 +0530

    committing first java class

---
+++ src/DemoJdbc.java
@@ -0,0 +1,6 @@
+public class DemoJdbc {
+    public static void main(String[] args) {
+        System.out.println("DemoJdbc class is running.");
+        // Additional JDBC code can be added here
+    }
+}
